53f2a42ef4ff95f56c970c453f0918ea3967b049
izarater <izarater@unal.edu.co>
2023-10-08 22:17:57 -0500

    routes changes for minor bug

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -17,10 +17,10 @@ const App = () => {
       <GlobalProvider>
         <Header />
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/iot" element={<IoT />} />
-          <Route exact path="/ai" element={<IA />} />
-          <Route exact path="/cc" element={<CC />} />
+          <Route exact path="TeIAPag/" element={<Home />} />
+          <Route exact path="TeIAPag/iot" element={<IoT />} />
+          <Route exact path="TeIAPag/ai" element={<IA />} />
+          <Route exact path="TeIAPag/cc" element={<CC />} />
           <Route exact path="*" element={<Home />} />
         </Routes>
       </GlobalProvider>
